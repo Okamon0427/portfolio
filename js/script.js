81df87f8cdccf2bdec60693a7fde55e09872afec
hemhem <takutookamoto0427@gmail.com>
2021-06-01 13:13:51 -0400

    Added typedjs

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var options = {
+  strings: ['a Web Developer', 'using a MERN stack', 'a sushi lover'],
+  typeSpeed: 60,
+  backSpeed: 40,
+  backDelay: 2000,
+  smartBackspace: false,
+  loop: true
+};
+var typed = new Typed('#typed', options);
